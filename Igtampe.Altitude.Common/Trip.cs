a5ee4035bc214a5e2e2c49f3c04e2c1f391cd392
Ignacio Tampe <Igtampe@gmail.com>
2022-04-14 12:41:15 -0400

    Add Public bool for sharing, Day management operations

--- Igtampe.Altitude.Common/Trip.cs
+++ Igtampe.Altitude.Common/Trip.cs
@@ -5,7 +5,7 @@ namespace Igtampe.Altitude.Common {
     
     /// <summary>A Trip of some sort to some place</summary>
     public class Trip : AutomaticallyGeneratableIdentifiable, Nameable, Describable, Depictable, Updateable, Ownable<User> {
-    
+
         /// <summary>Name of this trip</summary>
         public string Name { get; set; } = "";
 
@@ -24,6 +24,9 @@ namespace Igtampe.Altitude.Common {
         /// <summary>User who owns this trip</summary>
         public User? Owner { get; set; }
 
+        /// <summary>Whether or not this trip is public or not</summary>
+        public bool Public { get; set; } = false;
+
         /// <summary>The date this trip starts on</summary>
         public DateTime StartDate { get; set; } = DateTime.UtcNow;
 
@@ -41,6 +44,66 @@ namespace Igtampe.Altitude.Common {
             List<Warning> Warns = new();
             foreach (var item in Days) { Warns.AddRange(item.Validate()); }
             return Warns;
-        } 
+        }
+
+        /// <summary>Adds a day at the end of the list</summary>
+        /// <param name="D"></param>
+        public void AddDay(Day D) {
+            D.Index = Days.Count;
+            Days.Add(D);
+        }
+
+        /// <summary>Insert a day at a specific point</summary>
+        /// <param name="D"></param>
+        /// <param name="Index"></param>
+        public void AddDay(Day D, int Index) {
+            Days.Insert(Index, D); //Insert
+            RecalculateDayIndexes(Index);
+        }
+
+        /// <summary>Exchanges two days</summary>
+        /// <param name="Index"></param>
+        /// <param name="NewIndex"></param>
+        public void MoveDay(int Index, int NewIndex) {
+
+            //Ensure both indexes exist:
+            if(Index < 0 || Index >= Days.Count) throw new IndexOutOfRangeException("Original index does not exist");
+            if (NewIndex < 0 || NewIndex >= Days.Count) throw new InvalidOperationException("New Index does not exist");
+
+            //Mira we can do this with legitimately no additional vars (at least in this code)
+            //Watch:
+
+            Days[Index].Index = NewIndex;
+            Days[NewIndex].Index = Index;
+
+            Days = Days.OrderBy(A => A.Index).ToList();
+        
+        }
+
+        /// <summary>Moves a day up by one index</summary>
+        /// <param name="Index"></param>
+        public void MoveDayUp(int Index) => MoveDay(Index, Index - 1);
+
+        /// <summary>Moves a day down by one index</summary>
+        /// <param name="Index"></param>
+        public void MoveDayDown(int Index) => MoveDay(Index, Index + 1);
+
+        /// <summary>Removes a day</summary>
+        /// <param name="Index"></param>
+        public void RemoveDay(int Index) {
+            Days.RemoveAt(Index); //Insert
+            RecalculateDayIndexes(Index);
+        }
+
+        /// <summary>Moves an event to another day</summary>
+        /// <param name="DayIndex"></param>
+        /// <param name="EventIndex"></param>
+        /// <param name="OtherDayIndex"></param>
+        public void MoveEventToOtherDay(int DayIndex, int EventIndex, int OtherDayIndex) 
+            => Days[OtherDayIndex].AddEvent(Days[DayIndex].RemoveEvent(EventIndex));
+
+        private void RecalculateDayIndexes(int? Start = null) {
+            for (int i = Start ?? 0; i < Days.Count; i++) { Days[i].Index = i; } //Update indices
+        }
     }
 }
