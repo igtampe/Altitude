93f31e5759fde1e736b23cdf57fcea6a2dbdea62
Ignacio Tampe <Igtampe@gmail.com>
2022-04-14 16:16:08 -0400

    Migrations

---
+++ Igtampe.Altitude.Data/Migrations/20220414201542_DayColorIcon.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Igtampe.Altitude.Data.Migrations
+{
+    public partial class DayColorIcon : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "Day",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Icon",
+                table: "Day",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "Day");
+
+            migrationBuilder.DropColumn(
+                name: "Icon",
+                table: "Day");
+        }
+    }
+}
