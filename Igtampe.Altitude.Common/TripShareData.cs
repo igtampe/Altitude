d0ca5bf0a2f130be76b07f87c8dd7fc913e22d76
Ignacio Tampe <Igtampe@gmail.com>
2022-04-14 12:41:27 -0400

    Add Trip Share Data to be able to share trips

---
+++ Igtampe.Altitude.Common/TripShareData.cs
@@ -0,0 +1,13 @@
+﻿using Igtampe.ChopoAuth;
+
+namespace Igtampe.Altitude.Common {
+    /// <summary>Ownership data for a trip</summary>
+    public class TripShareData : AutomaticallyGeneratableIdentifiable {
+
+        /// <summary>Trip that's been shared</summary>
+        public Trip? Trip { get; set; }
+
+        /// <summary>User the trip was shared with</summary>
+        public User? User { get; set; }
+    }
+}
