88a3714b53f6b9f16446376af0f2e15f2f9879ae
Ignacio Tampe <Igtampe@gmail.com>
2022-04-14 01:20:47 -0400

    Add Migration

---
+++ Igtampe.Altitude.Data/Migrations/20220414050951_init.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Igtampe.Altitude.Data.Migrations {
+    /// <summary>Initial Migration to the DB</summary>
+    public partial class Init : Migration {
+
+        /// <summary>Puts this migration up</summary>
+        /// <param name="migrationBuilder"></param>
+        protected override void Up(MigrationBuilder migrationBuilder) {
+            migrationBuilder.CreateTable(
+                name: "Image",
+                columns: table => new {
+                    ID = table.Column<Guid>(type: "uuid", nullable: false),
+                    Data = table.Column<byte[]>(type: "bytea", nullable: true),
+                    Type = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table => table.PrimaryKey("PK_Image", x => x.ID));
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new {
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    ImageURL = table.Column<string>(type: "text", nullable: false),
+                    IsAdmin = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table => table.PrimaryKey("PK_User", x => x.Username));
+
+            migrationBuilder.CreateTable(
+                name: "Trip",
+                columns: table => new {
+                    ID = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    ImageURL = table.Column<string>(type: "text", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    OwnerUsername = table.Column<string>(type: "text", nullable: true),
+                    StartDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table => {
+                    table.PrimaryKey("PK_Trip", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Trip_User_OwnerUsername",
+                        column: x => x.OwnerUsername,
+                        principalTable: "User",
+                        principalColumn: "Username");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Day",
+                columns: table => new {
+                    ID = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    ImageURL = table.Column<string>(type: "text", nullable: false),
+                    Index = table.Column<int>(type: "integer", nullable: false),
+                    TripID = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table => {
+                    table.PrimaryKey("PK_Day", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Day_Trip_TripID",
+                        column: x => x.TripID,
+                        principalTable: "Trip",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Event",
+                columns: table => new {
+                    ID = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    ImageURL = table.Column<string>(type: "text", nullable: false),
+                    Index = table.Column<int>(type: "integer", nullable: false),
+                    Hour = table.Column<int>(type: "integer", nullable: false),
+                    Minute = table.Column<int>(type: "integer", nullable: false),
+                    ReminderTime = table.Column<int>(type: "integer", nullable: false),
+                    DayID = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table => {
+                    table.PrimaryKey("PK_Event", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Event_Day_DayID",
+                        column: x => x.DayID,
+                        principalTable: "Day",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Day_TripID",
+                table: "Day",
+                column: "TripID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Event_DayID",
+                table: "Event",
+                column: "DayID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trip_OwnerUsername",
+                table: "Trip",
+                column: "OwnerUsername");
+        }
+
+        /// <summary>Pulls this migration down</summary>
+        /// <param name="migrationBuilder"></param>
+        protected override void Down(MigrationBuilder migrationBuilder) {
+            migrationBuilder.DropTable(
+                name: "Event");
+
+            migrationBuilder.DropTable(
+                name: "Image");
+
+            migrationBuilder.DropTable(
+                name: "Day");
+
+            migrationBuilder.DropTable(
+                name: "Trip");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
